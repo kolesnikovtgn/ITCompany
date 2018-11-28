de03f72deb10947b088263b0cef9ee6ce8e96ea8
lodoss <develoer@lodosss-Mac-mini.local>
2018-11-28 14:58:16 +0300

    Added class Company for main work all project

--- itCompany.js
+++ itCompany.js
@@ -1,3 +1,26 @@
+class Director {
+    constructor() {
+        this.employees = [];
+        this.projects = [];
+    }
+
+    addEmployee(type) {
+        this.employees.push(new Employee(type));
+    }
+
+    deleteEmployee(id) {
+        this.employees.splice(id, 1);
+    }
+
+    getProjects() {
+        let countPrj = Math.floor(Math.random()*4);
+
+        for(let i=0; i<=countPrj; i++){
+            this.projects.push(new Project());
+        }
+    }
+}
+
 class Project {
     constructor(status='free') {
         this.id = this.getId();
@@ -15,6 +38,13 @@ class Project {
 
     changeStatus(status) {
         this.status = status;
+    }
+
+    developmentProject() {
+        for(let i=this.complexity; i>0; i--){
+            this.changeStatus('busy');
+        }
+        this.changeStatus('completed');
     }
 
     getComplexity() {
@@ -50,15 +80,31 @@ class Employee {
         this.type = type;
         this.experience = experience;
         this.status = status;
+        this.busyDay=0;
+        this.freeDay=0;
     }
 
     countExp() {
         this.experience++;
     }
 
-    changeStatus(status) {
-        this.status = status;
+    changeStatus() {
+        if (this.busyDay > 0) {
+            this.status = 'busy';
+        } else {
+            this.status = 'free';
+        }
     }
+
+    getProject(project) {
+        for(let i=project.complexity; i>0; i--){
+            this.busyDay++;
+        }
+        this.countExp();
+        this.changeStatus();
+        this.freeDay=0;
+    }
+
 
     getId() {
         let uuid = function () {
@@ -91,13 +137,27 @@ class Web extends Department {
         super();
 
         this.webEmployees = employees.filter((item) => {
-            return item.type=='web';
+            return item.type == 'web';
         });
         this.webProjects = projects.filter((item) => {
-            return item.type=='web';
+            return (item.type == 'web') && (item.status == 'free');
         });
     }
 
+    development() {
+        let webEmployesFree = this.webEmployees.filter((item) => {
+            return item.type == 'free';
+        });
+
+        for(let i=0; i < webEmployesFree.length; i++) {
+            // webEmployesFree[i].getProject();
+            // this.webProjects[i].developmentProject();
+        }
+
+        this.webProjects.forEach(() => {
+
+        });
+    }
 }
 
 class Mobile extends Department {
@@ -106,10 +166,10 @@ class Mobile extends Department {
         super();
 
         this.mobEmployees = employees.filter((item) => {
-            return item.type=='mob';
+            return item.type == 'mob';
         });
         this.mobProjects = projects.filter((item) => {
-            return item.type=='mob';
+            return (item.type == 'mob') && (item.status == 'free');
         });
     }
 
@@ -121,84 +181,39 @@ class Qa extends Department {
         super();
 
         this.qaEmployees = employees.filter((item) => {
-            return item.type=='qa';
+            return item.type == 'qa';
         });
         this.mobProjects = projects.filter((item) => {
-            return item.status=='completed';
+            return item.status == 'completed';
         });
 
+    }
 }
 
-class Director {
+class Company {
     constructor() {
-        this.employees = [];
-        this.projects = [];
+        this.director = new Director();
+        this.webDepartment = new Web(this.director.employees, this.director.projects);
+        this.mobDepartment = new Mobile(this.director.employees, this.director.projects);
+        this.qaDepartment = new Qa(this.director.employees, this.director.projects);
+
+
     }
 
-    addEmployee(type) {
-        this.employees.push(new Employee(type));
-    }
-
-    deleteEmployee(id) {
-        this.employees.splice(id, 1);
-    }
-
-    getProjects() {
-        let countPrj = Math.floor(Math.random()*4);
-
-        for(let i=0; i<=countPrj; i++){
-            this.projects.push(new Project());
+    work(allDays) {
+        for(let i=0; i<allDays; i++) {
+            this.director.getProjects();
+            // if(i==0) {console.log("lets GO!"); continue;} // Если это первый день - переходим на следующий день
         }
+        console.log(this.director.projects);
+        console.log("sdfsdfdsf");
+
+        console.log("sdfsdfdsf");
+        console.log(this.director.projects);
+        console.log(this.director.projects.length);
     }
+
 }
 
-let prj1 = new Project();
-console.log(prj1.id);
-console.log(prj1.type);
-console.log(prj1.status);
-prj1.changeStatus('qa');
-console.log(prj1.status);
-
-
-
-let prj2 = new Employee('mob');
-console.log(prj2.id);
-console.log(prj2.type);
-console.log(prj2.experience);
-prj2.countExp();
-prj2.countExp();
-console.log(prj2.experience);
-prj2.countExp();
-prj2.countExp();
-prj2.countExp();
-prj2.countExp();
-console.log(prj2.experience);
-prj2.changeStatus('busy');
-console.log(prj2.status);
-
-let prj3 = new Director();
-prj3.addEmployee('web');
-prj3.addEmployee('mobile');
-prj3.addEmployee('qa');
-prj3.addEmployee('web');
-console.log(prj3.employees);
-prj3.getProjects();
-console.log(prj3.projects);
-
-let prj4 = new Department(prj3.employees, prj3.projects);
-
-console.log("All Employes!!!!!!!!!!!!!");
-console.log(prj3.employees);
-console.log("All Employes!!!!!!!!!!!!!");
-console.log("=========================");
-console.log("Web Employes!!!!!!!!!!!!!");
-console.log(prj4.webEmployees);
-console.log("Web Employes!!!!!!!!!!!!!");
-console.log("-------------------------");
-console.log("All Projects!!!!!!!!!!!!!");
-console.log(prj3.projects);
-console.log("All Projects!!!!!!!!!!!!!");
-console.log("=========================");
-console.log("Web Projects!!!!!!!!!!!!!");
-console.log(prj4.webProjects);
-console.log("Web Projects!!!!!!!!!!!!!");
+let company = new Company();
+company.work(3);
